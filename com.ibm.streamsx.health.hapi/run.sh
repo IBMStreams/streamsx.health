bb3511c008b34fe26d7181476f681f1aa6bc90bf
chanskw <chanskw@streamsmgmt2.canlab.ibm.com>
2016-06-28 10:40:24 -0400

    Moved classes to internal, Added build by Gradle

---
+++ com.ibm.streamsx.health.hapi/run.sh
@@ -0,0 +1,11 @@
+$STREAMS_INSTALL/java/jre/bin/java \
+-Dfile.encoding=UTF-8 \
+-classpath com.ibm.streamsx.health.hapi.jar:$STREAMS_INSTALL/lib/com.ibm.streams.operator.jar:\
+$STREAMS_INSTALL/lib/com.ibm.streams.operator.samples.jar:$STREAMS_INSTALL/ext/lib/commons-math-2.2.jar:\
+$STREAMS_INSTALL/ext/lib/log4j-1.2.17.jar:$STREAMS_INSTALL/ext/lib/JSON4J.jar:$HAPI_HOME/lib/hapi-base-2.2.jar:\
+$HAPI_HOME/lib/hapi-structures-v21-2.2.jar:$HAPI_HOME/lib/hapi-structures-v22-2.2.jar:$HAPI_HOME/lib/hapi-structures-v23-2.2.jar:\
+$HAPI_HOME/lib/hapi-structures-v231-2.2.jar:$HAPI_HOME/lib/hapi-structures-v24-2.2.jar:\
+$HAPI_HOME/lib/hapi-structures-v25-2.2.jar:$HAPI_HOME/lib/hapi-structures-v251-2.2.jar:\
+$HAPI_HOME/lib/hapi-structures-v26-2.2.jar:$HAPI_HOME/lib/log4j-1.2.17.jar:$HAPI_HOME/lib/slf4j-api-1.6.6.jar:\
+$HAPI_HOME/lib/slf4j-log4j12-1.6.6.jar:$STREAMS_INSTALL/toolkits/com.ibm.streamsx.topology/opt/apache-mina-2.0.2/dist/mina-core-2.0.2.jar:\
+$STREAMS_INSTALL/toolkits/com.ibm.streamsx.topology/lib/com.ibm.streamsx.topology.jar com.ibm.streamsx.health.hapi.services.OruR01Ingest
