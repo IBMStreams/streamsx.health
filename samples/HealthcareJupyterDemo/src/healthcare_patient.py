d851a15b74887bec7aea0a13075cf480c9d68b53
streamsadmin <streamsadmin@streamsqse.localdomain>
2017-01-24 14:41:27 -0500

    Enable ANALYTICS_SERVICE context

--- samples/HealthcareJupyterDemo/src/healthcare_patient.py
+++ samples/HealthcareJupyterDemo/src/healthcare_patient.py
@@ -1,7 +1,8 @@
 import subprocess
+import json
 
 from streamsx.topology.topology import Topology, schema
-import streamsx.topology.context
+from streamsx.topology.context import ConfigParams, submit
 from streamsx.topology import functions
 
 from patientmonitoring_functions import RPeakDetect, RPeakAggregator
@@ -10,19 +11,20 @@ import healthcare_functions
 from windows import *
 
 class HealthcarePatientData:
-    def __init__(self, username, password, sample_rate=125, patient_id=None):
+    def __init__(self, vcap, username, password, sample_rate=125, patient_id=None):
+        self.vcap = vcap
         self.username = username
         self.password = password
         self.sample_rate = sample_rate
         self.target_sample_rate = 100 
         self.patient_id=patient_id
 
-    def run(self, context="DISTRIBUTED"):
+    def run(self, context='DISTRIBUTED'):
         ## Create topology
-        topo = Topology("HealthcareDemo")
+        topo = Topology('HealthcareDemo')
 
         ## Ingest, preprocess and aggregate patient data
-        patientData = topo.subscribe("ingest-physionet", schema.CommonSchema.Json) \
+        patientData = topo.subscribe('ingest-physionet', schema.CommonSchema.Json) \
                           .map(functions.identity) \
                           .filter(healthcare_functions.PatientFilter(self.patient_id)) \
                           .transform(healthcare_functions.GenTimestamp(self.sample_rate)) \
@@ -35,8 +37,16 @@ class HealthcarePatientData:
         ## Create a view of the data
         self.view_data = rpeak_data_stream.view()
 
+        ## Setup VCAP credential for remote build/submit
+        cfg = {}
+        if self.vcap and self.vcap.strip():
+          vcap_json = json.loads(self.vcap)
+          service_name = vcap_json['streaming-analytics'][0]['name']
+          cfg[ConfigParams.VCAP_SERVICES] = vcap_json
+          cfg[ConfigParams.SERVICE_NAME] = service_name  
+
         ## Compile Python Streams application and submit job
-        streamsx.topology.context.submit(context, topo.graph, username=self.username, password=self.password)
+        submit(context, topo.graph, config=cfg, username=self.username, password=self.password)
 
 
     '''Access view data'''
