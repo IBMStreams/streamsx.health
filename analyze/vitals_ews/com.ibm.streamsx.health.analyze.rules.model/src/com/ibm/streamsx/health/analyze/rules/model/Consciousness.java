af18643eea416dceb8ec0f88702588b4fcd58811
Samantha Chan <chanskw@ca.ibm.com>
2017-02-14 14:48:26 -0500

    31: ODM Rules for calculating EWS from patient's vitals

---
+++ analyze/vitals_ews/com.ibm.streamsx.health.analyze.rules.model/src/com/ibm/streamsx/health/analyze/rules/model/Consciousness.java
@@ -0,0 +1,11 @@
+package com.ibm.streamsx.health.analyze.rules.model;
+
+
+// Enum to represent patient's consciousness
+public enum Consciousness {
+	ALERT,
+	VOICE,
+	CONFUSE,
+	PAIN,
+	UNCONSCIOUS
+}
