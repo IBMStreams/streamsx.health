9d423000698087f3c7cc5f4ac401bbd36a93354c
James Cancilla <cancilla@ca.ibm.com>
2017-03-15 16:42:54 -0400

    Migrate Vitals EWS project

--- ingest/common/com.ibm.streamsx.health.ingest/impl/java/src/com/ibm/streamsx/health/ingest/types/resolver/ObservationTypeResolver.java
+++ ingest/common/com.ibm.streamsx.health.ingest/impl/java/src/com/ibm/streamsx/health/ingest/types/resolver/ObservationTypeResolver.java
@@ -1,5 +1,6 @@
 package com.ibm.streamsx.health.ingest.types.resolver;
 
+import com.ibm.streams.function.model.Function;
 import com.ibm.streamsx.health.ingest.types.model.Observation;
 import com.ibm.streamsx.health.ingest.types.model.ReadingTypeCode;
 
@@ -17,9 +18,9 @@ public class ObservationTypeResolver {
 	 *         waveform, `false` otherwise
 	 */
 	public static boolean isECGLeadI(Observation observation) {
-		return getCode(observation).equalsIgnoreCase(ReadingTypeCode.ECG_LEAD_I.getCode());
+		return isECGLeadI(getCode(observation));
 	}
-
+	
 	/**
 	 * Returns `true` if the given Observation object represents an ECG Lead II
 	 * waveform.
@@ -28,7 +29,7 @@ public class ObservationTypeResolver {
 	 *         waveform, `false` otherwise
 	 */
 	public static boolean isECGLeadII(Observation observation) {
-		return getCode(observation).equalsIgnoreCase(ReadingTypeCode.ECG_LEAD_II.getCode());
+		return isECGLeadII(getCode(observation));
 	}
 
 	/**
@@ -39,7 +40,7 @@ public class ObservationTypeResolver {
 	 *         waveform, `false` otherwise
 	 */
 	public static boolean isECGLeadIII(Observation observation) {
-		return getCode(observation).equalsIgnoreCase(ReadingTypeCode.ECG_LEAD_III.getCode());
+		return isECGLeadIII(getCode(observation));
 	}
 
 	/**
@@ -50,7 +51,7 @@ public class ObservationTypeResolver {
 	 *         waveform, `false` otherwise
 	 */
 	public static boolean isECGLeadV1(Observation observation) {
-		return getCode(observation).equalsIgnoreCase(ReadingTypeCode.ECG_LEAD_V1.getCode());
+		return isECGLeadV1(getCode(observation));
 	}
 
 	/**
@@ -61,7 +62,7 @@ public class ObservationTypeResolver {
 	 *         waveform, `false` otherwise
 	 */
 	public static boolean isECGLeadV2(Observation observation) {
-		return getCode(observation).equalsIgnoreCase(ReadingTypeCode.ECG_LEAD_V2.getCode());
+		return isECGLeadV2(getCode(observation));
 	}
 
 	/**
@@ -72,7 +73,7 @@ public class ObservationTypeResolver {
 	 *         waveform, `false` otherwise
 	 */
 	public static boolean isECGLeadV3(Observation observation) {
-		return getCode(observation).equalsIgnoreCase(ReadingTypeCode.ECG_LEAD_V3.getCode());
+		return isECGLeadV3(getCode(observation));
 	}
 
 	/**
@@ -83,7 +84,7 @@ public class ObservationTypeResolver {
 	 *         waveform, `false` otherwise
 	 */
 	public static boolean isECGLeadV4(Observation observation) {
-		return getCode(observation).equalsIgnoreCase(ReadingTypeCode.ECG_LEAD_V4.getCode());
+		return isECGLeadV4(getCode(observation));
 	}
 
 	/**
@@ -94,7 +95,7 @@ public class ObservationTypeResolver {
 	 *         waveform, `false` otherwise
 	 */
 	public static boolean isECGLeadV5(Observation observation) {
-		return getCode(observation).equalsIgnoreCase(ReadingTypeCode.ECG_LEAD_V5.getCode());
+		return isECGLeadV5(getCode(observation));
 	}
 
 	/**
@@ -105,7 +106,7 @@ public class ObservationTypeResolver {
 	 *         waveform, `false` otherwise
 	 */
 	public static boolean isECGLeadV6(Observation observation) {
-		return getCode(observation).equalsIgnoreCase(ReadingTypeCode.ECG_LEAD_V6.getCode());
+		return isECGLeadV6(getCode(observation));
 	}
 
 	/**
@@ -116,7 +117,7 @@ public class ObservationTypeResolver {
 	 *         waveform, `false` otherwise
 	 */
 	public static boolean isECGLeadV7(Observation observation) {
-		return getCode(observation).equalsIgnoreCase(ReadingTypeCode.ECG_LEAD_V7.getCode());
+		return isECGLeadV7(getCode(observation));
 	}
 
 	/**
@@ -127,7 +128,7 @@ public class ObservationTypeResolver {
 	 *         waveform, `false` otherwise
 	 */
 	public static boolean isECGLeadV8(Observation observation) {
-		return getCode(observation).equalsIgnoreCase(ReadingTypeCode.ECG_LEAD_V8.getCode());
+		return isECGLeadV8(getCode(observation));
 	}
 
 	/**
@@ -138,7 +139,7 @@ public class ObservationTypeResolver {
 	 *         waveform, `false` otherwise
 	 */
 	public static boolean isECGLeadV9(Observation observation) {
-		return getCode(observation).equalsIgnoreCase(ReadingTypeCode.ECG_LEAD_V9.getCode());
+		return isECGLeadV9(getCode(observation));
 	}
 
 	/**
@@ -149,7 +150,7 @@ public class ObservationTypeResolver {
 	 *         waveform, `false` otherwise
 	 */
 	public static boolean isECGLeadAVF(Observation observation) {
-		return getCode(observation).equalsIgnoreCase(ReadingTypeCode.ECG_LEAD_AVF.getCode());
+		return isECGLeadAVF(getCode(observation));
 	}
 
 	/**
@@ -160,7 +161,7 @@ public class ObservationTypeResolver {
 	 *         waveform, `false` otherwise
 	 */
 	public static boolean isECGLeadAVL(Observation observation) {
-		return getCode(observation).equalsIgnoreCase(ReadingTypeCode.ECG_LEAD_AVL.getCode());
+		return isECGLeadAVL(getCode(observation));
 	}
 
 	/**
@@ -171,7 +172,7 @@ public class ObservationTypeResolver {
 	 *         waveform, `false` otherwise
 	 */
 	public static boolean isECGLeadAVR(Observation observation) {
-		return getCode(observation).equalsIgnoreCase(ReadingTypeCode.ECG_LEAD_AVR.getCode());
+		return isECGLeadAVR(getCode(observation));
 	}
 
 	/**
@@ -182,7 +183,7 @@ public class ObservationTypeResolver {
 	 *         `false` otherwise
 	 */
 	public static boolean isHeartRate(Observation observation) {
-		return getCode(observation).equalsIgnoreCase(ReadingTypeCode.HEART_RATE.getCode());
+		return isHeartRate(getCode(observation));
 	}
 
 	/**
@@ -193,7 +194,7 @@ public class ObservationTypeResolver {
 	 *         vital, `false` otherwise
 	 */
 	public static boolean isRespiratoryRate(Observation observation) {
-		return getCode(observation).equalsIgnoreCase(ReadingTypeCode.RESP_RATE.getCode());
+		return isRespiratoryRate(getCode(observation));
 	}
 
 	/**
@@ -204,7 +205,7 @@ public class ObservationTypeResolver {
 	 *         `false` otherwise
 	 */
 	public static boolean isTemperature(Observation observation) {
-		return getCode(observation).equalsIgnoreCase(ReadingTypeCode.TEMPERATURE.getCode());
+		return isTemperature(getCode(observation));
 	}
 
 	/**
@@ -214,7 +215,7 @@ public class ObservationTypeResolver {
 	 *         `false` otherwise
 	 */
 	public static boolean isSpO2(Observation observation) {
-		return getCode(observation).equalsIgnoreCase(ReadingTypeCode.SPO2.getCode());
+		return isSpO2(getCode(observation));
 	}
 
 	/**
@@ -225,7 +226,7 @@ public class ObservationTypeResolver {
 	 *         Pressure vital, `false` otherwise
 	 */
 	public static boolean isBPSystolic(Observation observation) {
-		return getCode(observation).equalsIgnoreCase(ReadingTypeCode.BP_SYSTOLIC.getCode());
+		return isBPSystolic(getCode(observation));
 	}
 
 	/**
@@ -236,7 +237,7 @@ public class ObservationTypeResolver {
 	 *         Pressure vital, `false` otherwise
 	 */
 	public static boolean isBPDiastolic(Observation observation) {
-		return getCode(observation).equalsIgnoreCase(ReadingTypeCode.BP_DIASTOLIC.getCode());
+		return isBPDiastolic(getCode(observation));
 	}
 
 	/**
@@ -247,7 +248,7 @@ public class ObservationTypeResolver {
 	 *         `false` otherwise
 	 */
 	public static boolean isPleth(Observation observation) {
-		return getCode(observation).equalsIgnoreCase(ReadingTypeCode.PLETH.getCode());
+		return isPleth(getCode(observation));
 	}
 
 	/**
@@ -257,7 +258,7 @@ public class ObservationTypeResolver {
 	 *         `false` otherwise
 	 */
 	public static boolean isPulse(Observation observation) {
-		return getCode(observation).equalsIgnoreCase(ReadingTypeCode.PULSE.getCode());
+		return isPulse(getCode(observation));
 	}
 
 	/**
@@ -268,7 +269,293 @@ public class ObservationTypeResolver {
 	 *         the value for Observation.ReadingType.code
 	 */
 	public static String getLabel(Observation observation) {
-		String obsCode = getCode(observation);
+		return getLabel(getCode(observation));
+	}
+
+	/**
+	 * Returns `true` if the given Observation object represents an ECG Lead I
+	 * waveform.
+	 * 
+	 * @return `true` if the Observation object represents an ECG Lead I
+	 *         waveform, `false` otherwise
+	 */
+	@Function(namespace="com.ibm.streamsx.health.ingest.types.resolver")
+	public static boolean isECGLeadI(String code) {
+		return code.equalsIgnoreCase(ReadingTypeCode.ECG_LEAD_I.getCode());
+	}
+	
+	/**
+	 * Returns `true` if the given Observation object represents an ECG Lead II
+	 * waveform.
+	 * 
+	 * @return `true` if the Observation object represents an ECG Lead II
+	 *         waveform, `false` otherwise
+	 */
+	@Function(namespace="com.ibm.streamsx.health.ingest.types.resolver")
+	public static boolean isECGLeadII(String code) {
+		return code.equalsIgnoreCase(ReadingTypeCode.ECG_LEAD_II.getCode());
+	}
+
+	/**
+	 * Returns `true` if the given Observation object represents an ECG Lead III
+	 * waveform.
+	 * 
+	 * @return `true` if the Observation object represents an ECG Lead III
+	 *         waveform, `false` otherwise
+	 */
+	@Function(namespace="com.ibm.streamsx.health.ingest.types.resolver")
+	public static boolean isECGLeadIII(String code) {
+		return code.equalsIgnoreCase(ReadingTypeCode.ECG_LEAD_III.getCode());
+	}
+
+	/**
+	 * Returns `true` if the given Observation object represents an ECG Lead V1
+	 * waveform.
+	 * 
+	 * @return `true` if the Observation object represents an ECG Lead V1
+	 *         waveform, `false` otherwise
+	 */
+	@Function(namespace="com.ibm.streamsx.health.ingest.types.resolver")
+	public static boolean isECGLeadV1(String code) {
+		return code.equalsIgnoreCase(ReadingTypeCode.ECG_LEAD_V1.getCode());
+	}
+
+	/**
+	 * Returns `true` if the given Observation object represents an ECG Lead V2
+	 * waveform.
+	 * 
+	 * @return `true` if the Observation object represents an ECG Lead V2
+	 *         waveform, `false` otherwise
+	 */
+	@Function(namespace="com.ibm.streamsx.health.ingest.types.resolver")
+	public static boolean isECGLeadV2(String code) {
+		return code.equalsIgnoreCase(ReadingTypeCode.ECG_LEAD_V2.getCode());
+	}
+
+	/**
+	 * Returns `true` if the given Observation object represents an ECG Lead V3
+	 * waveform.
+	 * 
+	 * @return `true` if the Observation object represents an ECG Lead V3
+	 *         waveform, `false` otherwise
+	 */
+	@Function(namespace="com.ibm.streamsx.health.ingest.types.resolver")
+	public static boolean isECGLeadV3(String code) {
+		return code.equalsIgnoreCase(ReadingTypeCode.ECG_LEAD_V3.getCode());
+	}
+
+	/**
+	 * Returns `true` if the given Observation object represents an ECG Lead V4
+	 * waveform.
+	 * 
+	 * @return `true` if the Observation object represents an ECG Lead V4
+	 *         waveform, `false` otherwise
+	 */
+	@Function(namespace="com.ibm.streamsx.health.ingest.types.resolver")
+	public static boolean isECGLeadV4(String code) {
+		return code.equalsIgnoreCase(ReadingTypeCode.ECG_LEAD_V4.getCode());
+	}
+
+	/**
+	 * Returns `true` if the given Observation object represents an ECG Lead V5
+	 * waveform.
+	 * 
+	 * @return `true` if the Observation object represents an ECG Lead V5
+	 *         waveform, `false` otherwise
+	 */
+	@Function(namespace="com.ibm.streamsx.health.ingest.types.resolver")
+	public static boolean isECGLeadV5(String code) {
+		return code.equalsIgnoreCase(ReadingTypeCode.ECG_LEAD_V5.getCode());
+	}
+
+	/**
+	 * Returns `true` if the given Observation object represents an ECG Lead V6
+	 * waveform.
+	 * 
+	 * @return `true` if the Observation object represents an ECG Lead V6
+	 *         waveform, `false` otherwise
+	 */
+	@Function(namespace="com.ibm.streamsx.health.ingest.types.resolver")
+	public static boolean isECGLeadV6(String code) {
+		return code.equalsIgnoreCase(ReadingTypeCode.ECG_LEAD_V6.getCode());
+	}
+
+	/**
+	 * Returns `true` if the given Observation object represents an ECG Lead V7
+	 * waveform.
+	 * 
+	 * @return `true` if the Observation object represents an ECG Lead V7
+	 *         waveform, `false` otherwise
+	 */
+	@Function(namespace="com.ibm.streamsx.health.ingest.types.resolver")
+	public static boolean isECGLeadV7(String code) {
+		return code.equalsIgnoreCase(ReadingTypeCode.ECG_LEAD_V7.getCode());
+	}
+
+	/**
+	 * Returns `true` if the given Observation object represents an ECG Lead V8
+	 * waveform.
+	 * 
+	 * @return `true` if the Observation object represents an ECG Lead V8
+	 *         waveform, `false` otherwise
+	 */
+	@Function(namespace="com.ibm.streamsx.health.ingest.types.resolver")
+	public static boolean isECGLeadV8(String code) {
+		return code.equalsIgnoreCase(ReadingTypeCode.ECG_LEAD_V8.getCode());
+	}
+
+	/**
+	 * Returns `true` if the given Observation object represents an ECG Lead V9
+	 * waveform.
+	 * 
+	 * @return `true` if the Observation object represents an ECG Lead V9
+	 *         waveform, `false` otherwise
+	 */
+	@Function(namespace="com.ibm.streamsx.health.ingest.types.resolver")
+	public static boolean isECGLeadV9(String code) {
+		return code.equalsIgnoreCase(ReadingTypeCode.ECG_LEAD_V9.getCode());
+	}
+
+	/**
+	 * Returns `true` if the given Observation object represents an ECG Lead AVF
+	 * waveform.
+	 * 
+	 * @return `true` if the Observation object represents an ECG Lead AVF
+	 *         waveform, `false` otherwise
+	 */
+	@Function(namespace="com.ibm.streamsx.health.ingest.types.resolver")
+	public static boolean isECGLeadAVF(String code) {
+		return code.equalsIgnoreCase(ReadingTypeCode.ECG_LEAD_AVF.getCode());
+	}
+
+	/**
+	 * Returns `true` if the given Observation object represents an ECG Lead AVL
+	 * waveform.
+	 * 
+	 * @return `true` if the Observation object represents an ECG Lead AVL
+	 *         waveform, `false` otherwise
+	 */
+	@Function(namespace="com.ibm.streamsx.health.ingest.types.resolver")
+	public static boolean isECGLeadAVL(String code) {
+		return code.equalsIgnoreCase(ReadingTypeCode.ECG_LEAD_AVL.getCode());
+	}
+
+	/**
+	 * Returns `true` if the given Observation object represents an ECG Lead AVR
+	 * waveform.
+	 * 
+	 * @return `true` if the Observation object represents an ECG Lead AVR
+	 *         waveform, `false` otherwise
+	 */
+	@Function(namespace="com.ibm.streamsx.health.ingest.types.resolver")
+	public static boolean isECGLeadAVR(String code) {
+		return code.equalsIgnoreCase(ReadingTypeCode.ECG_LEAD_AVR.getCode());
+	}
+
+	/**
+	 * Returns `true` if the given Observation object represents a Heart Rate
+	 * vital.
+	 * 
+	 * @return `true` if the Observation object represents a Heart Rate vital,
+	 *         `false` otherwise
+	 */
+	@Function(namespace="com.ibm.streamsx.health.ingest.types.resolver")
+	public static boolean isHeartRate(String code) {
+		return code.equalsIgnoreCase(ReadingTypeCode.HEART_RATE.getCode());
+	}
+
+	/**
+	 * Returns `true` if the given Observation object represents a Respiratory
+	 * Rate vital.
+	 * 
+	 * @return `true` if the Observation object represents an Respiratory Rate
+	 *         vital, `false` otherwise
+	 */
+	@Function(namespace="com.ibm.streamsx.health.ingest.types.resolver")
+	public static boolean isRespiratoryRate(String code) {
+		return code.equalsIgnoreCase(ReadingTypeCode.RESP_RATE.getCode());
+	}
+
+	/**
+	 * Returns `true` if the given Observation object represents a Temperature
+	 * vital.
+	 * 
+	 * @return `true` if the Observation object represents a Temperature vital,
+	 *         `false` otherwise
+	 */
+	@Function(namespace="com.ibm.streamsx.health.ingest.types.resolver")
+	public static boolean isTemperature(String code) {
+		return code.equalsIgnoreCase(ReadingTypeCode.TEMPERATURE.getCode());
+	}
+
+	/**
+	 * Returns `true` if the given Observation object represents an SpO2 vital.
+	 * 
+	 * @return `true` if the Observation object represents an SpO2 vital,
+	 *         `false` otherwise
+	 */
+	@Function(namespace="com.ibm.streamsx.health.ingest.types.resolver")
+	public static boolean isSpO2(String code) {
+		return code.equalsIgnoreCase(ReadingTypeCode.SPO2.getCode());
+	}
+
+	/**
+	 * Returns `true` if the given Observation object represents a Systolic
+	 * Blood Pressure vital.
+	 * 
+	 * @return `true` if the Observation object represents a Systolic Blood
+	 *         Pressure vital, `false` otherwise
+	 */
+	@Function(namespace="com.ibm.streamsx.health.ingest.types.resolver")
+	public static boolean isBPSystolic(String code) {
+		return code.equalsIgnoreCase(ReadingTypeCode.BP_SYSTOLIC.getCode());
+	}
+
+	/**
+	 * Returns `true` if the given Observation object represents a Diastolic
+	 * Blood Pressure vital.
+	 * 
+	 * @return `true` if the Observation object represents a Diastolic Blood
+	 *         Pressure vital, `false` otherwise
+	 */
+	@Function(namespace="com.ibm.streamsx.health.ingest.types.resolver")
+	public static boolean isBPDiastolic(String code) {
+		return code.equalsIgnoreCase(ReadingTypeCode.BP_DIASTOLIC.getCode());
+	}
+
+	/**
+	 * Returns `true` if the given Observation object represents a Pleth
+	 * waveform.
+	 * 
+	 * @return `true` if the Observation object represents a Pleth waveform,
+	 *         `false` otherwise
+	 */
+	@Function(namespace="com.ibm.streamsx.health.ingest.types.resolver")
+	public static boolean isPleth(String code) {
+		return code.equalsIgnoreCase(ReadingTypeCode.PLETH.getCode());
+	}
+
+	/**
+	 * Returns `true` if the given Observation object represents a Pulse vital.
+	 * 
+	 * @return `true` if the Observation object represents a Pulse vital,
+	 *         `false` otherwise
+	 */
+	@Function(namespace="com.ibm.streamsx.health.ingest.types.resolver")
+	public static boolean isPulse(String code) {
+		return code.equalsIgnoreCase(ReadingTypeCode.PULSE.getCode());
+	}
+
+	/**
+	 * Returns the human-readable label of the reading type for the given
+	 * Observation object
+	 * 
+	 * @return the human readable label of the reading type, otherwise returns
+	 *         the value for Observation.ReadingType.code
+	 */
+	@Function(namespace="com.ibm.streamsx.health.ingest.types.resolver")
+	public static String getLabel(String code) {
+		String obsCode = code;
 		for (ReadingTypeCode readingTypeCode : ReadingTypeCode.values()) {
 			if (obsCode.equalsIgnoreCase(readingTypeCode.getCode())) {
 				return readingTypeCode.name().toLowerCase();
@@ -277,5 +564,5 @@ public class ObservationTypeResolver {
 
 		return obsCode;
 	}
-
+	
 }
