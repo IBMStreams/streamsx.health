53793f4a948fe83a4e3b72e4bed432bb7bffd792
kcibm <katrina.cecco@ibm.com>
2017-05-15 16:37:27 -0400

    Update setup.py

--- ingest/common/python/package/setup.py
+++ ingest/common/python/package/setup.py
@@ -6,7 +6,7 @@ setup(
   description = 'Utilities for running api course',
   author = 'IBM Streams @ github.com',
   
-  url = 'https://github.com/IBMStreams/streamsx.health',
+  url = 'https://github.com/kcibm/IBMStreams/streamsx.health',
   keywords = ['streams', 'ibmstreams', 'streaming', 'demo' ],
   classifiers = [
     'Development Status :: 3 - Alpha',
