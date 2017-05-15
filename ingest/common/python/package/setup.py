2eaa9486a29b42bb53f5058fb817ba48c8be1649
kcibm <katrina.cecco@ibm.com>
2017-05-15 16:33:57 -0400

    Create setup.py

---
+++ ingest/common/python/package/setup.py
@@ -0,0 +1,18 @@
+from distutils.core import setup
+setup(
+  name = 'streamsx.health',
+  packages = ['streamsx.health'],
+  version = '1.0',
+  description = 'Utilities for running api course',
+  author = 'IBM Streams @ github.com',
+  
+  url = 'https://github.com/IBMStreams/streamsx.health',
+  keywords = ['streams', 'ibmstreams', 'streaming', 'demo' ],
+  classifiers = [
+    'Development Status :: 3 - Alpha',
+    'License :: OSI Approved :: Apache Software License',
+    'Programming Language :: Python :: 3',
+    'Programming Language :: Python :: 3.5',
+  ],
+  install_requires=[],
+)
