8203f4fe18f64569b253ddabcae148cdf363a5f3
kcibm <katrina.cecco@ibm.com>
2017-05-30 19:12:44 -0400

    Python API Course Setup (#126)
    Allow python module to be discoverable

---
+++ ingest/common/python/package/setup.py
@@ -0,0 +1,18 @@
+from distutils.core import setup
+setup(
+  name = 'streamsx_health.ingest',
+  packages = ['streamsx_health.ingest'],
+  version = '1.0',
+  description = 'Utilities for running api course',
+  author = 'chanskw@ca.ibm.com',
+  
+  url = 'https://github.com/IBMStreams/streamsx.health',
+  keywords = ['streams', 'ibmstreams', 'streaming'],
+  classifiers = [
+    'Development Status :: 3 - Alpha',
+    'License :: OSI Approved :: Apache License 2.0',
+    'Programming Language :: Python :: 3',
+    'Programming Language :: Python :: 3.5',
+  ],
+  install_requires=[],
+)
