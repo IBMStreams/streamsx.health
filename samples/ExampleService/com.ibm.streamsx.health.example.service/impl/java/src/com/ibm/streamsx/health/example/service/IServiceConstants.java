580d28191b29b1b1020245328ea71f491941f4dc
Samantha Chan <chanskw@ca.ibm.com>
2017-10-10 19:35:48 +0000

    Create Example Service

---
+++ samples/ExampleService/com.ibm.streamsx.health.example.service/impl/java/src/com/ibm/streamsx/health/example/service/IServiceConstants.java
@@ -0,0 +1,15 @@
+//*******************************************************************************
+//* Copyright (C) 2017 International Business Machines Corporation
+//* All Rights Reserved
+//*******************************************************************************
+
+package com.ibm.streamsx.health.example.service;
+
+public interface IServiceConstants {
+	
+	// Keys in properties file
+	String KEY_DEBUG = "debug";
+	String KEY_STREAMS_CONTEXT = "streamscontext";
+	String KEY_VMARGS = "vmargs";
+
+}
