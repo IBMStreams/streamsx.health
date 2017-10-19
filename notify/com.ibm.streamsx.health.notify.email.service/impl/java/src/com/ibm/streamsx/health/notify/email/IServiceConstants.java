05605bb8a26369e479aabda4d476cd3124adf9ad
Samantha Chan <chanskw@ca.ibm.com>
2017-10-19 14:00:34 +0000

    Feature/emailnotification (#147)
    * Initial commit of email service
    
    * Update readme
    
    * Update readme
    
    * Update readme

---
+++ notify/com.ibm.streamsx.health.notify.email.service/impl/java/src/com/ibm/streamsx/health/notify/email/IServiceConstants.java
@@ -0,0 +1,15 @@
+//*******************************************************************************
+//* Copyright (C) 2017 International Business Machines Corporation
+//* All Rights Reserved
+//*******************************************************************************
+
+package com.ibm.streamsx.health.notify.email;
+
+public interface IServiceConstants {
+	
+	// Keys in properties file
+	String KEY_DEBUG = "debug";
+	String KEY_STREAMS_CONTEXT = "streamscontext";
+	String KEY_VMARGS = "vmargs";
+
+}
